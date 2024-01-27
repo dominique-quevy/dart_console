0389a0d0face217abf7be9ec5f91f99e72c2b527
Domdom <email_Domdom@email.com>
2024-01-27 23:55:29 +0100

    upgrade to dart 3.2.4

--- test/dart_console_test.dart
+++ test/dart_console_test.dart
@@ -2,7 +2,5 @@ import 'package:dart_console/dart_console.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('calculate', () => expect(calculate(), 42));
 }
